0221dac87a214874ae345cc428131d23111b7968
jersonsolorzano06 <jersolorzano06@gmail.com>
2025-07-09 13:01:45 -0500

    correccion config7

--- APII.js
+++ APII.js
@@ -9,13 +9,27 @@ exports.app = app;
 app.use(express.json());
 app.use(cors());
 
-app.get('/Base_de_datos_js/firebase-config.js', (req, res) => {
-  res.json({
-    apiKey: process.env.PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
-    // ... otras configs
-  });
+// APII.js (Backend - Firebase Admin SDK)
+const admin = require('firebase-admin');
+
+// Configuración desde variables de entorno (mejor práctica)
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+};
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_DATABASE_URL
 });
+
+module.exports = {
+  auth: admin.auth(),
+  db: admin.firestore(),
+  admin  // Opcional: exportar el admin completo
+};
+
 // Servir archivos estáticos desde la carpeta actual
 app.use(express.static(path.join(__dirname, '')));
 
--- Java/Base_de_datos_js/firebase-config.js
+++ Java/Base_de_datos_js/firebase-config.js
@@ -1,10 +1,11 @@
-const { app } = require("../APII.js");
+// const { app } = require("../../APII.js");
 
 // Configuración de Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCR2QOp1qGnPC9_EYbx_BJBZYhdiZknqKc",
   authDomain: "healthy-mind-d052a.firebaseapp.com",
   projectId: "healthy-mind-d052a",
+
   storageBucket: "healthy-mind-d052a.firebasestorage.app",
   messagingSenderId: "157316594591",
   appId: "1:157316594591:web:db9200e589d6a87643dbaa",
